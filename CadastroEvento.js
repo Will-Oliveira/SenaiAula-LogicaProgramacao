8c269de6f1a29b879e5d4fbb7a3f616d14a43d6c
Will-Oliveira <william_p_oliveira@outlook.com>
2022-02-23 22:41:26 -0300

    ultima atualizacao d cadastroEvento

--- CadastroEvento.js
+++ CadastroEvento.js
@@ -1,22 +1,24 @@
 // para verificar o código mudando de acordo com os devidos input, é preciso mudar valores nas linhas descritas abaixo
-// linha 13, altera a data que o evento irá acontecer - destaco que a formatação da data está esabelecida em: "mês/dia/ano"
-// linha 28, altera a data de nascimento - destaco que a formatação da data está esabelecida em: "mês/dia/ano"
-// linha 37, para incluir um nome extra na lista (ou retirar), é preciso que faça colocando o nome entre aspas e inserir virgula: "nome da pessoa",
-// linha 39, altere a quantidade de lugares que terá o evento
-// linha 45, altere a quantidade de lugares que será feita a condicional
+// linha 14, altera a data que o evento irá acontecer - destaco que a formatação da data está esabelecida em: "mês/dia/ano"
+// linha 29, altera a data de nascimento - destaco que a formatação da data está esabelecida em: "mês/dia/ano"
+// linha 38, coloque seu nome para incluir na listagem de participantes
+// linha 41, para incluir um nome extra na lista (ou retirar), é preciso que escreva o nome entre aspas e inserir virgula: "nome da pessoa", isso irá afetar a soma!
+// linha 43, altere a quantidade de lugares que terá o evento
+// linha 49, altere a quantidade de lugares que será feita a condicional
 
-// algumas observações:
-// a linha 16 contém o código que pesquisei no seguinte link: https://www.youtube.com/watch?v=2EAs2BH3Y_4&t=79s
-// nas linhas 11, 12, 13 e 14, devido ainda não ter chegado no módulo para estudar essa função, pesquisei (MUITO) e encontrei um código bem pequeno, no link: 
+// algumas observações e devidos créditos:
+// as linhas 18 e 20 contém o código que usei para sanar o problema de formatação para pt-BR; pesquisei e achei a solução no seguinte link: https://www.youtube.com/watch?v=2EAs2BH3Y_4&t=79s
+// nas linhas 23, 24, 25 e 26; devido ainda não ter chegado no módulo para estudar essa função, pesquisei (MUITO) e encontrei um código bem pequeno, no link abaixo que sanou meu problema: 
 // https://pt.stackoverflow.com/questions/93741/calcular-idade-em-anos-usando-javascript
 
 let dataEvento = new Date('03/30/2022')
 let dataHoje = new Date()
 
 if(dataEvento > dataHoje){
-    console.log("Olá, o evento ocorrerá dia: " + (new Intl.DateTimeFormat('pt-BR', {day: 'numeric', month: 'long', year: 'numeric'}).format(dataEvento) + "\nFavor, insira sua data de nascimento a seguir:\n"))
+    console.log("Olá, hoje é " + (new Intl.DateTimeFormat('pt-BR', {day: 'numeric', month: 'long', year: 'numeric'}).format(dataHoje)) + ".\nA palestra será: " + (new Intl.DateTimeFormat('pt-BR', {day: 'numeric', month: 'long', year: 'numeric'}).format(dataEvento) + ".\nFavor, insira sua data de nascimento a seguir:\n"))
+    
 } else {
-    console.log("Que pena, o evento ocorreu dia:", dataEvento, "Acompanhe nosso site que em breve, teremos outros eventos. \n")
+    console.log("Que pena, o evento ocorreu dia:" + (new Intl.DateTimeFormat('pt-BR', {day: 'numeric', month: 'long', year: 'numeric'}).format(dataEvento) + "\nMas não se preocupe! Acompanhe nosso site que em breve, teremos outros eventos. \n"))
 }
 
 function idade(d1, d2) {
@@ -25,18 +27,21 @@ function idade(d1, d2) {
 	return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
 }
 
-  	let dataAniversario = (idade(new Date('03/10/2020')))
+let dataAniversario = (idade(new Date('03/10/2000')))
 
   if(dataAniversario > 17){
-	  console.log("Parabéns! Você atende a idade minima para se cadastrar. A seguir, coloque seu nome na lista de participantes.\n") 
-  }
+	  console.log("Parabéns! Você atende a idade mínima para se cadastrar. A seguir, coloque seu nome na lista de participantes.\n") 
+}
   else{
 	  console.log("Cadastro não efetuado. Este evento é voltado para maiores de 18 anos.\n")
-  }
-    console.log("Abaixo, segue a lista de particiantes que irão participar da palestra, junto com você:")
-    let listaParticipantes = ["Miguel", "Arthur", "Gael", "Heitor", "Theo", "Davi", "Gabriel", "Bernardo", "Samuel", "João Miguel", "Helena", "Alice", "Maria Alice"]
+}
+
+let seuNome = "William"
+    console.log("Olá", seuNome, "\nAbaixo, segue a lista de particiantes que irão participar da palestra, junto com você:\n")
+
+let listaParticipantes = [seuNome, "Miguel", "Arthur", "Gael", "Heitor", "Theo", "Davi", "Gabriel", "Bernardo", "Samuel", "João Miguel", "Helena", "Alice", "Maria Alice"]
     quantidade = listaParticipantes.length;
-    let lugares = 100
+let lugares = 100
     
     for(let contador = 0; contador < listaParticipantes.length; contador++){
         console.log(listaParticipantes[contador])}
